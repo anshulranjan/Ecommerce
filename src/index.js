b87c49f53fca8c5077a8f7a1fcb9e4d1dd797192
anshulranjan <ranjananshul1@gmail.com>
2021-09-05 15:21:16 +0530

    Ant design & Menubar

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import {BrowserRouter} from "react-router-dom";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import "antd/dist/antd.css";
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
