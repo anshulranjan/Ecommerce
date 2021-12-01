7216f0c4d3a019dcbb61b0e90f98935e2eee9411
anshulranjan <ranjananshul1@gmail.com>
2021-12-01 20:16:29 +0530

    Price Filters

--- src/components/search/Shop.js
+++ src/components/search/Shop.js
@@ -3,14 +3,18 @@ import { getProductsByCount, fetchByFilters } from "../../functions/product";
 import { useSelector, useDispatch } from "react-redux";
 import { SearchProductCard } from "./SearchProductCard";
 import {SearchLoadingCard} from "./SearchLoadingCard";
-import { Card, Typography, Row, Pagination } from 'antd';
+import { Menu, Slider, Row, Pagination } from 'antd';
 import {CloseOutlined} from "@ant-design/icons";
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [price, setPrice]= useState([0,0]);
+    const [ok, setOk] = useState(false);
+    let dispatch = useDispatch();
     const {search} = useSelector((state)=> ({... state}));
     const {text} = search;
+    const {SubMenu, ItemMenu} = Menu;
     const arr = new Array(4);
     var elements=[];
     for(var i=0;i<arr.length;i++){
@@ -28,6 +32,7 @@ const Shop = () => {
             setLoading(false);
         })
     };
+
     //load product on user search input
     useEffect(() => {
         const delayed = setTimeout(() => {
@@ -52,13 +57,52 @@ const Shop = () => {
         }
     };
 
+    //load products with price
+    useEffect (() => {
+        fetchProducts({price});
+
+    },[ok])
+
+    const handleSlider = (value) => {
+        dispatch({
+            type:"SEARCH_QUERY",
+            payload:{ text: "" }
+        });
+        setPrice(value);
+        setTimeout(() => {
+            setOk(!ok);
+        },300)
+    }
+
+    const fetchProducts = (arg) => {
+        fetchByFilters(arg).then((res) => {
+          setProducts(res.data);
+          setLoading(false);
+        });
+      };
+
+    
+
+
     return (
         <div className="container-fluid">
             <div className="row">
-                <div className="col-md-3">
-                    Search/Filter Menu
+                <div className="col-md-3 p-4">
+                    <Menu defaultOpenKeys={["1"]} mode="inline">
+                    <span className="h5">Filters</span>
+                    <SubMenu key="1" title={<span className="h6">PRICE</span>}>
+                        <div>
+                            <Slider className="ml-4 mr-4" tipFormatter={(v) => `Rs. ${v}`} range value={price} onChange={handleSlider} max="200000" />
+                        </div>
+                    </SubMenu>
+                    </Menu>
                 </div>
+
+
+
+
                 <div className="col-md-9">
+                    <h4 className="p-4">Products</h4>
                     <Row>
                     { loading &&  elements}
                     {!loading && products.length<1 && 
